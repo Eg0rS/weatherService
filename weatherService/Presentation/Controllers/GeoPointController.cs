135d0cde6cdd9b4f1a52663273ded797e2f8c814
Egor Salnikov <Ehrgit@yandex.ru>
2023-09-19 06:03:39 +0300

    bam

--- weatherService/Presentation/Controllers/GeoPointController.cs
+++ weatherService/Presentation/Controllers/GeoPointController.cs
@@ -35,7 +35,6 @@ public class GeoPointController : ControllerBase
         return Ok(geoPointsDto);
     }
 
-
     [HttpPost]
     public async Task<IActionResult> CreateGeoPoint(Guid userId, [FromBody] GeoPointForCreationDto geoPointForCreationDto)
     {
